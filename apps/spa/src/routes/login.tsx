ca54b30cdd53f6bbabb3058ea4173d36d3f154da
Stephen Lacy <me@slacy.me>
2025-04-16 10:06:38 -0700

    chore: example of using shared package in client

--- apps/spa/src/routes/login.tsx
+++ apps/spa/src/routes/login.tsx
@@ -1,26 +1,39 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { signIn } from '../lib/auth-client.ts'
+import { add } from '@workspace/lib'
+import { useState } from 'react'
 
 export const Route = createFileRoute('/login')({
 	component: RouteComponent,
 })
 
 function RouteComponent() {
+	const [number, setNumber] = useState(0)
+
+	const addNumbers = () => {
+		setNumber(add(1, number))
+	}
+
 	return (
 		<div className='flex h-full justify-center items-center'>
-			<button
-				type='button'
-				onClick={async () => {
-					await signIn.magicLink({
-						email: 'dev@runreal.dev',
-						// Use the dashboard url
-						callbackURL: 'http://localhost:5173/',
-					})
-				}}
-			>
-				{' '}
-				Send Magic Link{' '}
-			</button>
+			<div className='grid grid-cols-1 gap-4'>
+				<button
+					type='button'
+					onClick={async () => {
+						await signIn.magicLink({
+							email: 'dev@runreal.dev',
+							// Use the dashboard url
+							callbackURL: 'http://localhost:5173/',
+						})
+					}}
+				>
+					{' '}
+					Send Magic Link{' '}
+				</button>
+				<button type='button' onClick={() => addNumbers()}>
+					Number: {number}
+				</button>
+			</div>
 		</div>
 	)
 }
